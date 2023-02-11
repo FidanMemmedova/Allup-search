a1073c9938ecdcc01a632baab5d3f6a2fa15a27e
FidanMemmedova <5TAWDL9@code.edu.az>
2023-02-11 23:33:57 +0400

    salam

---
+++ P133Allup/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using P133Allup.DataAccessLayer;
+
+namespace P133Allup.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AppDbContext _context;
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        } 
+        public IActionResult Search(string searchedProduct)
+        {
+            if (string.IsNullOrEmpty(searchedProduct.Trim()))
+            {
+                return NoContent();
+            }
+            var product=_context.Products.Where(x=>x.Title.Contains(searchedProduct)).ToList();
+            return PartialView("_SharedPartialView", product);
+        }
+    }
+}
---
+++ P133Allup/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using P133Allup.DataAccessLayer;
+
+namespace P133Allup.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly AppDbContext _context;
+        public ProductController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        } 
+        public async Task<IActionResult> Detail(int id)
+        {
+            var product = await _context.Products.SingleOrDefaultAsync(x => x.Id == id);
+            return View(product);
+        }
+    }
+}
